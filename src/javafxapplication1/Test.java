f24df3117ca4e1b7c9b04ff7ef13e185a442c9bd
Bertrand Brompton <bertrandbrompton@gmail.com>
2014-02-08 18:13:41 +0000

    initial alpha codebase

---
+++ src/javafxapplication1/Test.java
@@ -0,0 +1,202 @@
+package javafxapplication1;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.VBoxBuilder;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Test extends Application {
+    private Stage primaryStage;
+    final Label message = new Label("");
+    final PasswordField pb = new PasswordField();
+    final TextField uf = new TextField();
+    final CheckBox checkbox = new CheckBox("Remember Username");
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        //this.primaryStage.setFullScreen(true);
+        Button btn = new Button("Login");
+        btn.setOnAction(loginClienteHandler());
+         
+        checkbox.setSelected(true);
+        Button btn2 = new Button("Forgot your password");
+        btn2.setOnAction(forgotPasswordHandler());
+        Button btn3 = new Button("Create new Account");
+        btn3.setOnAction(createNewAccountHandler());
+        //PasswordField passwordField = new PasswordField();
+        //passwordField.setPromptText("Your password");
+        StackPane root = new StackPane();
+ //       root.getChildren().add(btn);
+        //root.getChildren().add(passwordField);
+
+        Scene scene = new Scene(root, 800, 600);
+        //Media media = new Media("background.flv");
+        //MediaPlayer mediaplayer = new MediaPlayer(media);
+        //mediaplayer.setAutoPlay(true);
+        //MediaView mediaview = new MediaView(mediaplayer);
+        //root.getChildren().add(mediaview);
+
+        VBox vb = new VBox();
+        vb.setPadding(new Insets(10, 0, 0, 10));
+        vb.setSpacing(10);
+        vb.setStyle("-fx-background-color: #344152; -fx-effect: dropshadow( one-pass-box , black , 8 , 0.0 , 2 , 0 ); ");
+        
+        vb.setPrefSize(300, 50);
+        
+        HBox hb = new HBox();
+        hb.setSpacing(10);
+        hb.setAlignment(Pos.CENTER_LEFT);
+        
+        Label unLabel = new Label("Username");
+        unLabel.setTextFill(Color.web("#ffffff"));
+        
+        Label pwLabel = new Label("Password");
+        pwLabel.setTextFill(Color.web("#ffffff"));
+        
+        Label lolisfree = new Label("Playing League of Legends is FREE!");
+        lolisfree.setTextFill(Color.web("#ffffff"));
+
+        Label accountlogin = new Label("Account Login");
+        accountlogin.setTextFill(Color.web("#ffffff"));
+        
+        HBox hb2 = new HBox();
+        
+        
+        pb.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                if (!pb.getText().equals("pass")) {
+                    message.setText("Your password is incorrect!");
+                    message.setTextFill(Color.rgb(210, 39, 30));
+                } else {
+                    message.setText("Your password has been confirmed");
+                    message.setTextFill(Color.rgb(21, 117, 84));
+                }
+                pb.clear();
+            }
+        });
+        String image = Test.class.getResource("background.jpg").toExternalForm();
+        root.setStyle("-fx-background-image: url('" + image + "'); -fx-background-position: center center; -fx-background-repeat: stretch;");          		
+//        Image imagez = new Image("background.jpg");
+//        ImageView iv1 = new ImageView();
+//        iv1.setImage(imagez);
+//        iv1.setFitWidth(300);
+//        iv1.setPreserveRatio(true);
+//        iv1.setSmooth(true);
+//        iv1.setCache(true);
+
+        
+        hb.getChildren().addAll(pwLabel, pb);
+        hb2.getChildren().addAll(unLabel, uf);
+        vb.getChildren().addAll(accountlogin,hb2, hb, checkbox, message, btn, btn2,btn3,lolisfree);
+        BorderPane border = new BorderPane();
+        border.setPadding(new Insets(20, 0, 20, 20));
+        border.setLeft(vb);
+        //root.getChildren().add(hb);
+        root.getChildren().add(border);
+        
+        hb2.setSpacing(10);
+        //root.getChildren().add(hb2);
+        
+        
+        
+        primaryStage.setTitle("League of Legends Login Client");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public EventHandler loginClienteHandler() {
+        EventHandler evh = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if ((pb.getText().equals("pass")) && (uf.getText().equals("mouse"))) {
+                    message.setText("Your username and password has been confirmed");
+                    message.setTextFill(Color.rgb(21, 117, 84));
+                    gameClient gc = new gameClient();
+                    gc.start(primaryStage);
+                    
+                } else {
+                    message.setText("Your username and password is incorrect!");
+                    message.setTextFill(Color.rgb(210, 39, 30));
+
+                }
+                
+                if(checkbox.isSelected() == false){
+                	uf.clear();
+                }
+                pb.clear();
+            }
+        };
+        return evh;
+    }
+    public EventHandler forgotPasswordHandler(){
+        EventHandler evh = new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+            //@Override
+            //public void handle(ActionEvent event) {
+            //		Application.getHostServices().showDocument(url);
+            //}
+        };
+        return evh;    	
+    }
+    public EventHandler playHandler(){
+        EventHandler evh = new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+            //@Override
+            //public void handle(ActionEvent event) {
+            //		Application.getHostServices().showDocument(url);
+            //}
+        };
+        return evh;    	
+    }    
+    public EventHandler createNewAccountHandler(){
+        EventHandler evh = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                primaryStage.setScene(new Scene(VBoxBuilder.create().children(new Text("text")).build()));
+                primaryStage.sizeToScene();
+                primaryStage.setFullScreen(false);
+                primaryStage.setFullScreen(true);
+            }
+        };
+        return evh;    	
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/javafxapplication1/gameClient.java
@@ -0,0 +1,173 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javafxapplication1;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author bertrandbrompton
+ */
+public class gameClient {
+    public Stage primaryStage;
+    public void start(Stage stage){
+        String menustyle = "-fx-padding: 12 30 12 30; -fx-focus-color: firebrick;";
+        primaryStage = stage;
+        StackPane gamestack = new StackPane();
+        Scene gameclient = new Scene(gamestack, 800,600);
+
+        VBox friendlist = new VBox();
+        friendlist.setPadding(new Insets(10, 0, 0, 10));
+        friendlist.setSpacing(10);
+        friendlist.setStyle(menustyle);               
+        friendlist.setPrefSize(300, 50);
+
+        HBox menu = new HBox();
+        menu.setPadding(new Insets(10, 0, 0, 10));
+        menu.setSpacing(10);
+        menu.setStyle(menustyle);                
+        menu.setPrefSize(300, 50);
+
+        HBox chatpanel = new HBox();
+        chatpanel.setPadding(new Insets(10, 0, 0, 10));
+        chatpanel.setSpacing(10);
+        chatpanel.setStyle(menustyle);               
+        chatpanel.setPrefSize(300, 50);
+
+        Button friendsbtn = new Button("Friends");
+        friendsbtn.setOnAction(playHandler());
+        chatpanel.getChildren().add(friendsbtn);                   
+        friendsbtn.setStyle(menustyle); //-fx-background-insets: 0,1,2,3;
+        friendsbtn.setAlignment(Pos.TOP_CENTER);
+
+        Button communitybtn = new Button("Community");
+        communitybtn.setOnAction(playHandler());
+        chatpanel.getChildren().add(communitybtn);
+        communitybtn.setAlignment(Pos.CENTER_RIGHT);
+        communitybtn.setStyle(menustyle);
+
+        Button notificationsbtn = new Button("Notifications");
+        notificationsbtn.setOnAction(playHandler());
+        chatpanel.getChildren().add(notificationsbtn);
+        notificationsbtn.setAlignment(Pos.CENTER_RIGHT);
+        notificationsbtn.setStyle(menustyle);
+
+        BorderPane border = new BorderPane();
+        border.setPadding(new Insets(20, 0, 20, 20));
+        //border.setRight(friendlist);
+        border.setTop(menu);
+        border.setBottom(chatpanel);
+
+        Button mainbtn = new Button("Main");
+        mainbtn.setOnAction(playHandler());
+        menu.getChildren().add(mainbtn);
+        mainbtn.setStyle(menustyle);
+
+        Button playbtn = new Button("Play");
+        playbtn.setOnAction(playHandler());
+        menu.getChildren().add(playbtn);                 
+        playbtn.setStyle(menustyle);
+
+        Button shopbtn = new Button("Shop");
+        shopbtn.setOnAction(playHandler());
+        menu.getChildren().add(shopbtn);
+        shopbtn.setStyle(menustyle);
+
+        Button profilebtn = new Button("Profile");
+        profilebtn.setOnAction(playHandler());
+        menu.getChildren().add(profilebtn);
+        profilebtn.setStyle(menustyle);
+
+        Button lorebtn = new Button("Lore");
+        lorebtn.setOnAction(playHandler());
+        menu.getChildren().add(lorebtn);
+        lorebtn.setStyle("-fx-padding: 12 30 12 30;");
+
+        Button helpbtn = new Button("Help");
+        helpbtn.setOnAction(playHandler());
+        menu.getChildren().add(helpbtn);
+        helpbtn.setStyle(menustyle);
+
+        Button optionsbtn = new Button("Options");
+        optionsbtn.setOnAction(playHandler());
+        menu.getChildren().add(optionsbtn);
+        optionsbtn.setStyle(menustyle);
+
+        GridPane gridpane = new GridPane();
+        Label labela = new Label();
+        labela.setText("Featured Item 1");
+        labela.setStyle("-fx-text-fill: white; -fx-font-size: 12px;");
+
+        Label labelb = new Label();
+        labelb.setText("Featured Item 2");
+        labelb.setStyle("-fx-text-fill: white; -fx-font-size: 12px;");
+
+        Label labelc = new Label();
+        labelc.setText("Featured Item 3");
+        labelc.setStyle("-fx-text-fill: white; -fx-font-size: 12px;");
+
+        Label labeld = new Label();
+        labeld.setText("Featured Item 4");
+        labeld.setStyle("-fx-text-fill: white; -fx-font-size: 12px;");
+
+
+        GridPane.setConstraints(labela, 1,1);
+        GridPane.setConstraints(labelb, 1,2);
+        GridPane.setConstraints(labelc, 2,1);
+        GridPane.setConstraints(labeld, 2,2);
+        gridpane.getChildren().addAll(labela,labelb,labelc,labeld);
+        gridpane.getColumnConstraints().add(new ColumnConstraints(300));
+        gridpane.getColumnConstraints().add(new ColumnConstraints(300));
+        gridpane.getRowConstraints().add(new RowConstraints(300));
+        gridpane.getRowConstraints().add(new RowConstraints(300));
+
+        border.setCenter(gridpane);
+
+
+        gamestack.getChildren().add(border);
+
+
+
+        primaryStage.setScene(gameclient);
+        String image = Test.class.getResource("background2.jpg").toExternalForm();
+        gamestack.setStyle("-fx-background-image: url('" + image + "'); -fx-background-position: center center; -fx-background-repeat: stretch;");
+        //primaryStage.setScene(new Scene(VBoxBuilder.create().children(new Text("League of Legends Client")).build()));
+        primaryStage.sizeToScene();
+        primaryStage.setFullScreen(false);
+        primaryStage.setFullScreen(true);
+    }
+    public EventHandler playHandler(){
+        EventHandler evh = new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+            //@Override
+            //public void handle(ActionEvent event) {
+            //		Application.getHostServices().showDocument(url);
+            //}
+        };
+        return evh;    	
+    }
+    
+}
