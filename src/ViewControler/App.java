528300ae2f11f565f296689f1ffd043cae3768c6
Bertrand Brompton <bertrandbrompton@gmail.com>
2014-02-11 11:11:21 +0000

    Added a comment describing the class

--- src/ViewControler/App.java
+++ src/ViewControler/App.java
@@ -1,9 +1,11 @@
 package ViewControler;
 
-
 import javafx.application.Application;
 import javafx.stage.Stage;
-
+/**
+ * This class is the first one run when the application starts. As it currently stands it starts the Login Menu. This might be subject to change if we include a client patcher.
+ * @author bertrandbrompton
+ */
 public class App extends Application {
     private Stage primaryStage;
 
